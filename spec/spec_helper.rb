979cabdbadf38dfe52cd108fa1c21323912573de
Chris Lucas <chris@chrisjlucas.com>
2014-04-24 22:02:16 -0700

    [spec] added Coveralls support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,20 @@
+require 'simplecov'
+require 'coveralls'
+
+class SimpleFormatter
+  def format(result)
+    puts "Coverage: #{result.covered_lines} / #{result.total_lines} LOC (#{result.covered_percent.round(2)}%) covered."
+  end
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleFormatter,
+    Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
 require 'rspec'
 
 require 'net/nntp'
